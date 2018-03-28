1ef836d0656e4125ed58e8d93650fc51b7e00767
J2b2590 <31115299+J2b2590@users.noreply.github.com>
2018-03-28 13:37:34 -0500

    working on lift component

--- app/assets/javascripts/components/lifts.es6.jsx
+++ app/assets/javascripts/components/lifts.es6.jsx
@@ -2,17 +2,19 @@ class Lifts extends React.Component {
 	constructor(props){
 		super(props)
 		this.state = {
-			lifts: [],
+			data: this.props.data,
+			lift: []
 
 			
-		}
+		};
 	}
   render () {
-  	debugger;
+  	// debugger;
     return (
     	<div className="lifts">
     		<h1 className="title">Lift</h1>
     	</div>
+    	
     );
   }
 }
