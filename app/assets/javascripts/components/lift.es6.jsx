f6bad34991cd3f8282472e4bd98fc75f4c849a96
J2b2590 <31115299+J2b2590@users.noreply.github.com>
2018-04-03 00:51:42 -0500

    workingon data table

--- app/assets/javascripts/components/lift.es6.jsx
+++ app/assets/javascripts/components/lift.es6.jsx
@@ -8,7 +8,11 @@ class Lift extends React.Component{
 				<tr>
 					
 						<td key={this.props.lifts.date}>{this.props.lifts.date}</td>
-					
+						<td key={this.props.lifts.liftname}>{this.props.lifts.liftname}</td>
+						<td key={this.props.lifts.ismetric}>{this.props.lifts.ismetric}</td>
+						<td key={this.props.lifts.weightlifted}>{this.props.lifts.weightlifted}</td>
+						<td key={this.props.lifts.repsperformed}>{this.props.lifts.repsperformed}</td>
+						<td key={this.props.lifts.onrm}>{this.props.lifts.onrm}</td>
 			    </tr>
 			</tbody>
 		</table>
